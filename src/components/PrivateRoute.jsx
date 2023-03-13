778bf55ab852fa42016f0a932192c48676c5c9ef
Ayoub Saadaoui <saadaouiayoub.08@gmail.com>
2023-03-13 01:18:01 +0100

    create the LoaderImage component

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,11 @@
 import { Navigate, Outlet } from "react-router"
 import { useAuthStatus } from "../hooks/useAuthStatus"
+import LoaderImage from "./LoaderImage";
 
 export default function PrivateRoute() {
     const { loggedIn, checkingStatus } = useAuthStatus();
     if (checkingStatus) {
-        return <h3>Loading...</h3>
+        return <LoaderImage />
     }
   return loggedIn ? <Outlet/> : <Navigate to="/sign-in" />
 }
